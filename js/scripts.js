65afc486af2a541a6034664953cbb88a2463171b
Fedois <fedefox14@gmail.com>
2023-01-12 14:03:50 +0100

    aggiunta bombe

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,61 @@
 // alert('per iniziare a giocare clicca su "play"')
 
 const container = document.querySelector('.container');
-console.log(container)
-
-const arrayN = [];
+const arrayBomb = [];
+console.log('numero bombe:', arrayBomb)
+const arrayNum = [];
+console.log('numero celle:', arrayNum);
 
 const play = document.querySelector('button')
 play.addEventListener('click', function(){
     
-    container.innerHTML = ''
-    square = createSquare()
+container.innerHTML = ''
+    
+    for(let i = 1; i <= 100; i++){
 
+        const square = createSquare(i)
+        container.append(square)
+
+        arrayNum.push(i)
+    
+    }
 })
 
+for(let j = 1; j <= 16; j++){
+    let bomb = getRandomNumber(1, 16);
+
+    while(arrayBomb.includes(bomb)){
+        
+        bomb = getRandomNumber(1, 16);
+
+    }
+        
+    arrayBomb.push(bomb);
+
+}
 
 
 // FUNCTION
 
-function createSquare () {
-for(let i = 1; i <= 100; i++){
+function createSquare(number) {
     
     const newSquare = document.createElement('div');
     newSquare.classList.add('square');
-    newSquare.innerHTML = i;
-    container.append(newSquare)
+    
 
     newSquare.addEventListener('click', function () {
 
-        if(this.classList.contains('click')){
-            this.classList.remove('click')
-        }
-        else{
-            this.classList.add('click')
-            console.log('cliccato sul numero: ', i)
-        }
-        return newSquare 
+        this.classList.add('click');
+        console.log(this.innerText);
+        
     })
-   
-}
+
+    newSquare.innerHTML = number;
+    
+    return newSquare 
 }
 
 function getRandomNumber(min, max) {
+    
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
